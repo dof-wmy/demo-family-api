5cbe065815669530b7068cadc70ab07d86e9cf10
WMY <mengyang1229@gmail.com>
2019-03-17 23:39:31 +0800

    Notice $fillable

--- app/Models/Notice.php
+++ app/Models/Notice.php
@@ -4,6 +4,11 @@ namespace App\Models;
 
 class Notice extends Base
 {
+    protected $fillable = [
+        'title',
+        'content',
+    ];
+
     public function user(){
         $this->belongsTo(User::class);
     }
