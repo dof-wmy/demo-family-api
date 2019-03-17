5d92e47c62682749a79656e96519c1a670ea7b69
WMY <mengyang1229@gmail.com>
2019-03-17 19:20:23 +0800

    UserRoleInformation fillable

--- app/Models/UserRoleInformation.php
+++ app/Models/UserRoleInformation.php
@@ -4,5 +4,14 @@ namespace App\Models;
 
 class UserRoleInformation extends Base
 {
-    //
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'role_id',
+        'key',
+        'value',
+    ];
 }
