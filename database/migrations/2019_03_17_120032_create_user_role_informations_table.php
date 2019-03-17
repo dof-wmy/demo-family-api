8ab3a526a3aa198c96541ecfff6f954113b7ee72
WMY <mengyang1229@gmail.com>
2019-03-17 17:13:12 +0800

    CreateUserRoleInformationsTable

--- database/migrations/2019_03_17_120032_create_user_role_informations_table.php
+++ database/migrations/2019_03_17_120032_create_user_role_informations_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUserRoleInfoTable extends Migration
+class CreateUserRoleInformationsTable extends Migration
 {
     /**
      * Run the migrations.
