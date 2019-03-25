e1622b06b5108768243c58fad0670a89e193cebc
WMY <mengyang1229@gmail.com>
2019-03-25 15:24:27 +0800

    Base getCreatedAtAttribute

--- app/Models/Base.php
+++ app/Models/Base.php
@@ -4,7 +4,11 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
+use Carbon\Carbon;
+
 class Base extends Model
 {
-    //
+    public function getCreatedAtAttribute($value){
+        return $value ? Carbon::parse($value)->format('Y-m-d H:i') : null;
+    }
 }
