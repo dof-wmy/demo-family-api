e56363295b3bacd39d49ae6607f0e378a3a080fc
WMY <mengyang1229@gmail.com>
2019-04-13 09:28:47 +0800

    App\Console\Commands\Test

---
+++ app/Console/Commands/Test.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class Test extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'test';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = '测试命令';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        $this->test();
+    }
+
+    private function test(){
+        $this->info('test');
+    }
+
+}
